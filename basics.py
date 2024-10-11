725cf0474d1a084f3cf2e9b7dfaedcf6c28eb5c0
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-10-11 11:13:55 +0600

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,20 @@
+
+# comment: use hash symbol
+# shortcut: ctrl + /
+# how to use double quotation under double quotation?
+# use backward slash before double quoation
+# example:
+print("In the bleak \"Midwinter\"")
+print("Hey", 7, 8)
+
+# sep: separates each word
+print("Hey", 7, 8, sep="~")
+print("Hey", 7, 8, sep="&")
+# print("Hey", a, b) is not correct
+# correct version:
+print("Hey", 7, 8, sep="&")
+print("Hey a b")
+
+# end: specify what to print at the end
+print("Hey", 7, 8, sep="~",end=" like this")
+
