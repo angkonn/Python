72ea3bd2ef0b5de509a571b700092da327399214
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-10-11 11:32:31 +0600

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,29 @@
+fruits=['apple','banana','orange']
+for x in fruits: 
+    print(x)
+    
+for n in range(0,2):{
+print(n)}
+    
+    # even numbers: 
+for n in range(0,11,2):{
+print(n)}
+
+
+# conditional statements:
+age=10;
+if (age>=18):
+    print("You can enter")
+else:
+    print("You cannot start")
+
+# By User input 
+name=str(input("What's your name? "))
+print(name)
+age=int(input("What's your age? "))
+print(age)
+Ticket= bool(input("If yes, type: True. If no, type: False "))
+if ((age>=18)and (Ticket== True )):
+    print("You can enter")
+else:
+    print("You cannot start")
